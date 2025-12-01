a9a08b2c673a328c6c3eadbc1ec89d504b425540
Nikhil Jaroli <jarolinikhil@gmail.com>
2025-12-01 21:10:18 +0530

    Operator2

---
+++ Operator2.java
@@ -0,0 +1,9 @@
+public class Operator2 {
+     public static void main(String[] args) {
+        int a = 10;     // first number
+        int b = 20;     // second number
+        int sub= a - b;  // subtracting numbers
+
+        System.out.println("result = " + sub);
+    }
+}
