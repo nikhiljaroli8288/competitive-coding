de6e6960ab4b42f91c675ea5c7af3607dfc2a11f
Nikhil Jaroli <jarolinikhil@gmail.com>
2025-12-01 21:05:27 +0530

    Add Operator.java

---
+++ Operator1.java
@@ -0,0 +1,9 @@
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 10;     // first number
+        int b = 20;     // second number
+        int sum = a + b;  // adding numbers
+
+        System.out.println("Sum = " + sum);
+    }
+}
