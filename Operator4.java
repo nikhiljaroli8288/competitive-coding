7ddbd28f70ee4215af8868b810dc1d84d71d6445
Nikhil Jaroli <jarolinikhil@gmail.com>
2025-12-02 16:58:41 +0530

    maximum of 2 numbers

---
+++ Operator4.java
@@ -0,0 +1,16 @@
+ //find maximum of 2 numbers
+import java.util.Scanner;
+public class Operator4 {
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    if(a>b){
+        System.out.println(a+" is grater than "+b);
+    }
+    else{
+        System.out.println(b+" is grater than "+a);
+    }
+    sc.close();
+}
+}
