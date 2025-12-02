a35d48ef3444a7e9ec7c413284c69baf49764409
Nikhil Jaroli <jarolinikhil@gmail.com>
2025-12-02 17:24:12 +0530

    Swappin 2 numbers

---
+++ Operator6.java
@@ -0,0 +1,20 @@
+// swapping 2 numbers
+import java.util.*;
+
+public class Operator6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
+    }
+}
