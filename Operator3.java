59d97a9f2f723988526dd2420b8b8a20d8bf11df
Nikhil Jaroli <jarolinikhil@gmail.com>
2025-12-02 07:47:55 +0530

    Operator3

---
+++ Operator3.java
@@ -0,0 +1,13 @@
+
+
+    public class Operator3 {
+    public static void main(String[] args) {
+        int n=15;
+        if ((n & 1) == 1)
+            System.out.println("Odd");
+        else
+            System.out.println("Even");
+    }
+    
+}
+
