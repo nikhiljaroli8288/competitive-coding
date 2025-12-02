f3c475db5aaca8c64335630643f4d5589563d9e2
nikhiljaroli8288 <jarolinikhil@gmail.com>
2025-12-02 16:45:15 +0530

    Update Operator3.java

--- Operator3.java
+++ Operator3.java
@@ -1,12 +1,15 @@
 
-
+//even odd checker
+import java.util.Scanner;
     public class Operator3 {
     public static void main(String[] args) {
-        int n=15;
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
         if ((n & 1) == 1)
             System.out.println("Odd");
         else
             System.out.println("Even");
+        sc.close();
     }
     
 }
