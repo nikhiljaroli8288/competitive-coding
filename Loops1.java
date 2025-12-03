f2f55ad5ed8bd30364200299ff765192a51f6fa2
Nikhil Jaroli <jarolinikhil@gmail.com>
2025-12-03 21:29:51 +0530

    print numbers from 1 to 10(using for loop)

---
+++ Loops1.java
@@ -0,0 +1,8 @@
+// print numbers from 1 to 10
+public class Loops1 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++)
+            System.out.print(i + " ");
+    }
+}
+
