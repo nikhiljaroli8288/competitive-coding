9c9de6a6e87301579e3ff340b2010476d65d17f6
Nikhil Jaroli <jarolinikhil@gmail.com>
2025-12-04 07:40:53 +0530

    file

---
+++ nikhil.java
@@ -0,0 +1,21 @@
+
+// electricity bill
+import java.util.Scanner;
+
+public class nikhil {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter units: ");
+        int units = sc.nextInt();
+        int bill = 0;
+
+        if(units <= 100)
+            bill = units * 5;
+        else if(units <= 200)
+            bill = (100 * 5) + (units - 100) * 7;
+        else
+            bill = (100 * 5) + (100 * 7) + (units - 200) * 10;
+
+        System.out.println("Total Bill = " + bill);
+    }
+}
